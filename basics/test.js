b5af20846a11e61890c36c6adf7e776c5525fe83
mynameparv <parvajain327p@gmail.com>
2025-05-25 05:16:53 +0000

    Add basics/test.js

---
+++ basics/test.js
@@ -0,0 +1,14 @@
+console.log("parva jain")
+
+const heros =["parv","dishan","divyansh","shree"]
+let myObj = {
+    name : "parv",
+    age : 22,
+}
+
+console.table(myObj);
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
